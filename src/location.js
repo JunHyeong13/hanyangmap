8898c3b95de7f2d70967bb447b37cde4cf88d165
JunHyeong13 <whwnsgud13@hanyang.ac.kr>
2023-10-18 13:59:34 +0900

    Modify the position Marker on the Map

--- src/location.js
+++ src/location.js
@@ -1,8 +1,17 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 const MapComponent = () => {
   const mapContainerRef = useRef(null);
   const mapRef = useRef(null);
+  const [markersVisible, setMarkersVisible] = useState(true);
+
+  const showMarkers = () => {
+    setMarkersVisible(true);
+  };
+
+  const hideMarkers = () => {
+    setMarkersVisible(false);
+  };
 
   useEffect(() => {
     if (mapContainerRef.current) {
@@ -11,56 +20,68 @@ const MapComponent = () => {
         level: 3
       };
 
+      // 현재 위치 기준으로 보여줌. (참조 : useEffect)
       const map = new window.kakao.maps.Map(mapContainerRef.current, mapOption);
       mapRef.current = map;
 
-      // Map Type Control
+      // 일반 지도 형태 | 위성 지도 형태
       const mapTypeControl = new window.kakao.maps.MapTypeControl();
       map.addControl(mapTypeControl, window.kakao.maps.ControlPosition.TOPRIGHT);
 
-      // Zoom Control
+      // 지도 줌인/줌아웃
       const zoomControl = new window.kakao.maps.ZoomControl();
       map.addControl(zoomControl, window.kakao.maps.ControlPosition.RIGHT);
 
-      // Create a marker position
-      const position = new window.kakao.maps.LatLng(37.298761981595376,126.8379862598931);
+      // 마커를 원하는 지점에 클릭하여 생성
+      function addMarker(position) {
+        const marker = new window.kakao.maps.Marker({
+          position: position
+        });
+        marker.setMap(markersVisible ? map : null);
 
-      // Create a marker
-      const marker = new window.kakao.maps.Marker({
-        position: position
-      });
-
-      // Add the marker to the map
-      marker.setMap(map);
-
-      // Create an info window content
-      const iwContent = '<div style="padding:6px;">Hanyang Shuttle</div>';
-
-      // Create an info window
+        // 마커 이벤트 1 (마우스를 마커 가까이 오버레이 했을 떄 보여짐)
+        window.kakao.maps.event.addListener(marker, 'mouseover', function() {
+            infowindow.open(map, marker);
+            });
+        
+        // 마커 이벤트 2 (마우스를 마커로부터 멀어지게하면 보이지 않음)
+        window.kakao.maps.event.addListener(marker, 'mouseout', function() {
+            infowindow.close();
+            });
+      }
+      
+      // 마커 위에 보일 수 있는 info 만들기
+      const infoContents = '<div style="padding:10px;">ERICA Shuttle</div>';
       const infowindow = new window.kakao.maps.InfoWindow({
-        content: iwContent
+        content: infoContents
       });
+      
+      // 처음 마커가 생성되어 보이는 위치 
+      addMarker(new window.kakao.maps.LatLng(37.298761981595376, 126.8379862598931));
 
-      // Add a mouseover event to the marker
-      window.kakao.maps.event.addListener(marker, 'mouseover', function() {
-        // When the mouse is over the marker, display the info window
-        infowindow.open(map, marker);
-      });
-
-      // Add a mouseout event to the marker
-      window.kakao.maps.event.addListener(marker, 'mouseout', function() {
-        // When the mouse is out of the marker, close the info window
-        infowindow.close();
+      // Click event to add a marker
+      window.kakao.maps.event.addListener(map, 'click', function (mouseEvent) {
+        addMarker(mouseEvent.latLng);
       });
     }
-  }, []);
+  }, [markersVisible]);
 
   return (
     <div
       ref={mapContainerRef}
       id="map"
-      style={{ width: '100%', height: '400px' }}
-    ></div>
+      style={{ width: '80%', height: '600px' }}
+    >
+      {markersVisible ? (
+        <div>
+          <button onClick={hideMarkers}>마커 감추기</button>
+        </div>
+      ) : (
+        <div>
+          <button onClick={showMarkers}>마커 보이기</button>
+        </div>
+      )}
+    </div>
   );
 };
 
