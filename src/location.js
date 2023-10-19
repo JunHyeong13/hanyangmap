5d307e5b475c3b10f18f72f2f617029462cf5f50
JunHyeong13 <whwnsgud13@hanyang.ac.kr>
2023-10-19 21:18:55 +0900

    Make the Marker + Address information on the map

--- src/location.js
+++ src/location.js
@@ -1,31 +1,8 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef } from 'react';
 
 const MapComponent = () => {
   const mapContainerRef = useRef(null);
   const mapRef = useRef(null);
-  const [markersVisible, setMarkersVisible] = useState(true);
-  const [activeMarker, setActiveMarker] = useState(null);
-  const [infoWindow, setInfoWindow] = useState(null);
-
-  const showMarkers = () => {
-    setMarkersVisible(true);
-    if (activeMarker) {
-      activeMarker.setMap(mapRef.current);
-      if (infoWindow) {
-        infoWindow.open(mapRef.current, activeMarker);
-      }
-    }
-  };
-
-  const hideMarkers = () => {
-    setMarkersVisible(false);
-    if (activeMarker) {
-      activeMarker.setMap(null);
-      if (infoWindow) {
-        infoWindow.close();
-      }
-    }
-  };
 
   useEffect(() => {
     if (mapContainerRef.current) {
@@ -37,75 +14,65 @@ const MapComponent = () => {
       const map = new window.kakao.maps.Map(mapContainerRef.current, mapOption);
       mapRef.current = map;
 
-      const mapTypeControl = new window.kakao.maps.MapTypeControl();
-      map.addControl(mapTypeControl, window.kakao.maps.ControlPosition.TOPRIGHT);
+      let openInfoWindow = null;
 
-      const zoomControl = new window.kakao.maps.ZoomControl();
-      map.addControl(zoomControl, window.kakao.maps.ControlPosition.RIGHT);
-
-      function addMarker(position) {
+      // Function to add a marker and display the address
+      function addMarkerAndAddress(position) {
+        // Create a marker
         const marker = new window.kakao.maps.Marker({
           position: position
         });
 
-        marker.setMap(markersVisible ? map : null);
-
+        // Event listener for marker click
         window.kakao.maps.event.addListener(marker, 'click', function() {
-          if (marker.getMap() === null) {
-            marker.setMap(map);
-            setActiveMarker(marker);
+          // Create a geocoder instance
+          const geocoder = new window.kakao.maps.services.Geocoder();
 
-            if (infoWindow) {
-              infoWindow.close();
-            }
+          // Get the address based on the clicked position
+          geocoder.coord2Address(position.getLng(), position.getLat(), function(result, status) {
+            if (status === window.kakao.maps.services.Status.OK) {
+              // Create infoContents with address and a link for directions
+              const infoContents = '<div style="padding:10px;">' + result[0].address.address_name +
+                '<br><a href="https://map.kakao.com", ' + position.getLat() + ',' + position.getLng() +
+                ' style="color:blue" target="_blank">길찾기</a> </div>';
+              
+              // Create an infoWindow with the infoContents
+              const infoWindow = new window.kakao.maps.InfoWindow({
+                position: position,
+                content: infoContents
+              });
 
-            const geocoder = new window.kakao.maps.services.Geocoder();
-            geocoder.coord2Address(position.getLng(), position.getLat(), function(result, status) {
-              if (status === window.kakao.maps.services.Status.OK) {
-                const infoContents = '<div style="padding:10px;">' + result[0].address.address_name + '</div>';
-                const newInfoWindow = new window.kakao.maps.InfoWindow({
-                  content: infoContents
-                });
-                newInfoWindow.open(map, marker);
-                setInfoWindow(newInfoWindow);
-              } else {
-                console.error('Geocoding failed with status:', status);
+              // Close the previously open infoWindow if there is one
+              if (openInfoWindow) {
+                openInfoWindow.close();
               }
-            });
-          } else {
-            marker.setMap(null);
-            setActiveMarker(null);
-            if (infoWindow) {
-              infoWindow.close();
-              setInfoWindow(null);
+
+              // Open the new infoWindow and update the openInfoWindow variable
+              infoWindow.open(map, marker);
+              openInfoWindow = infoWindow;
+            } else {
+              console.error('Geocoding failed with status:', status);
             }
-          }
+          });
         });
+
+        // Add the marker to the map
+        marker.setMap(map);
       }
 
-      addMarker(new window.kakao.maps.LatLng(37.298761981595376, 126.8379862598931));
-
-      window.kakao.maps.event.addListener(map, 'click', function (mouseEvent) {
-        addMarker(mouseEvent.latLng);
+      // Event listener to add a marker and display the address on map click
+      window.kakao.maps.event.addListener(map, 'click', function(mouseEvent) {
+        addMarkerAndAddress(mouseEvent.latLng);
       });
     }
-  }, [infoWindow, markersVisible]);
+  }, []);
 
   return (
-    <div style={{ display: 'flex', flexDirection: 'column-reverse' }}>
-      <div>
-        {markersVisible ? (
-          <button onClick={hideMarkers}>Hide markers</button>
-        ) : (
-          <button onClick={showMarkers}>Show markers</button>
-        )}
-      </div>
-      <div
-        ref={mapContainerRef}
-        id="map"
-        style={{ width: '80%', height: '600px' }}
-      ></div>
-    </div>
+    <div
+      ref={mapContainerRef}
+      id="map"
+      style={{ width: '80%', height: '600px' }}
+    ></div>
   );
 };
 
